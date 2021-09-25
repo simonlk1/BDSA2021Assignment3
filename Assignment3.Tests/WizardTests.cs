263b4ec4386f3a57683c8d770cfba2e43ed19d4a
rkri <rkri@itu.dk>
2021-09-25 13:27:35 +0200

    Fixed the Wizards.csv file and implemented the method for wizards invented by " " using linq

--- Assignment3.Tests/WizardTests.cs
+++ Assignment3.Tests/WizardTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment3.Tests
@@ -5,16 +6,26 @@ namespace Assignment3.Tests
     public class WizardTests
     {
         [Fact]
-        public void Wizards_contains_2_wizards()
+        public void Wizards_contains_12_wizards()
         {
             var wizards = Wizard.Wizards.Value;
 
-            Assert.Equal(2, wizards.Count);
+            Assert.Equal(12, wizards.Count);
         }
 
         [Theory]
         [InlineData("Darth Vader", "Star Wars", 1977, "George Lucas")]
         [InlineData("Sauron", "The Fellowship of the Ring", 1954, "J.R.R. Tolkien")]
+        [InlineData("Harry Potter","Harry Potter and the Philosopher's Stone",2001,"J.K. Rowling")]
+        [InlineData("Ron Weasley","Harry Potter and the Philosopher's Stone",2001,"J.K. Rowling")]
+        [InlineData("Albus Dumbledore","Harry Potter and the Chamber of Secrets",2002,"J.K. Rowling")]
+        [InlineData("Draco Malfoy","Harry Potter and the Chamber of Secrets",2002,"J.K. Rowling")]
+        [InlineData("Darth Maul","Star Wars",1977,"George Lucas")]
+        [InlineData("Frodo","The Fellowship of the Ring",1954,"J.R.R. Tolkien")]
+        [InlineData("Darth Sidious","Star Wars Episode 5",1980,"George Lucas")]
+        [InlineData("Sam","The Fellowship of the Ring",1954,"J.R.R. Tolkien")]
+        [InlineData("Doctor Strange","Doctor Strange",2016,"Scott Derrickson")]
+        [InlineData("Merlin","Merlin",1998,"Steve Barron")]
         public void Spot_check_wizards(string name, string medium, int year, string creator)
         {
             var wizards = Wizard.Wizards.Value;
@@ -25,6 +36,20 @@ namespace Assignment3.Tests
                 w.Year == year &&
                 w.Creator == creator
             );
+        }
+
+        [Fact]
+        public void WizardsInventedByRowling_Return_Harry_Potter_Ron_Weasley_Albus_Dumbledore_Draco_Malfoy()
+        {
+            // arrange
+            var wizards = Wizard.Wizards.Value;
+            
+            // act
+            var actual = Queries.GetWizardsInventedByAuthor("Rowling",wizards);
+
+            // assert
+            var expected = new []{"Harry Potter","Ron Weasley","Albus Dumbledore","Draco Malfoy"};
+            Assert.Equal(expected,actual);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Assignment3
 {
     public class Queries
     {
+        public static IEnumerable<string> GetWizardsInventedByAuthor(string author, IEnumerable<Wizard> wizards)
+        {
+            var wizardsFiltered = wizards.Where(w => w.Creator.Contains(author)).Select(w => w.Name); 
+            return wizardsFiltered;
+        }
+        
     }
 }

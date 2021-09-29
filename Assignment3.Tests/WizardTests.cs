fee339e51d39bcf0c85718e32163494e9e67a6a4
RasmusUK <74377458+RasmusUK@users.noreply.github.com>
2021-09-29 12:09:55 +0200

    Linq exercise 3 completed (#4)
    * Added 10 wizards in Wizards.csv
    
    * Fixed the Wizards.csv file and implemented the method for wizards invented by " " using linq
    
    * Querie for invented by also implemented as extension method
    
    * #2 linq and extension method implemented and tested for first year sith lord was introduced
    
    * Implemented and tests #3 list of wizards from Harry Potter books as value tuple of name and year
    
    * Implemented and test #4 list of wizard names grouped by creator in reverse order by creator name and then wizard name
    
    * All tests are now passing
    
    Co-authored-by: rkri <rkri@itu.dk>

--- Assignment3.Tests/WizardTests.cs
+++ Assignment3.Tests/WizardTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment3.Tests
@@ -5,16 +6,26 @@ namespace Assignment3.Tests
     public class WizardTests
     {
         [Fact]
-        public void Wizards_contains_2_wizards()
+        public void Wizards_contains_12_wizards()
         {
             var wizards = Wizard.Wizards.Value;
 
-            Assert.Equal(2, wizards.Count);
+            Assert.Equal(12, wizards.Count);
         }
 
         [Theory]
         [InlineData("Darth Vader", "Star Wars", 1977, "George Lucas")]
         [InlineData("Sauron", "The Fellowship of the Ring", 1954, "J.R.R. Tolkien")]
+        [InlineData("Harry Potter","Harry Potter and the Philosopher's Stone",2001,"J.K. Rowling")]
+        [InlineData("Ron Weasley","Harry Potter and the Philosopher's Stone",2001,"J.K. Rowling")]
+        [InlineData("Albus Dumbledore","Harry Potter and the Chamber of Secrets",2002,"J.K. Rowling")]
+        [InlineData("Draco Malfoy","Harry Potter and the Chamber of Secrets",2002,"J.K. Rowling")]
+        [InlineData("Darth Maul","Star Wars",1977,"George Lucas")]
+        [InlineData("Frodo","The Fellowship of the Ring",1954,"J.R.R. Tolkien")]
+        [InlineData("Darth Sidious","Star Wars Episode 5",1980,"George Lucas")]
+        [InlineData("Sam","The Fellowship of the Ring",1954,"J.R.R. Tolkien")]
+        [InlineData("Doctor Strange","Doctor Strange",2016,"Scott Derrickson")]
+        [InlineData("Merlin","Merlin",1998,"Steve Barron")]
         public void Spot_check_wizards(string name, string medium, int year, string creator)
         {
             var wizards = Wizard.Wizards.Value;
@@ -25,6 +36,123 @@ namespace Assignment3.Tests
                 w.Year == year &&
                 w.Creator == creator
             );
+        }
+
+        [Fact]
+        public void WizardsInventedByRowlingUsingLinq_Return_Harry_Potter_Ron_Weasley_Albus_Dumbledore_Draco_Malfoy()
+        {
+            // act
+            var actual = Queries.GetWizardsInventedByRowlingLinq();
+
+            // assert
+            var expected = new []{"Harry Potter","Ron Weasley","Albus Dumbledore","Draco Malfoy"};
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void WizardsInventedByRowlingUsingExtensionMethod_Return_Harry_Potter_Ron_Weasley_Albus_Dumbledore_Draco_Malfoy()
+        {
+            // act
+            var actual = Queries.GetWizardsInventedByRowlingExtensionMethod();
+
+            // assert
+            var expected = new []{"Harry Potter","Ron Weasley","Albus Dumbledore","Draco Malfoy"};
+            Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void FirstYearSithLordWasIntroduced_Return_1977_UsingLinq()
+        {
+            // act
+            var actual = Queries.GetYearTheFirstSithLordWasIntroducedLinq();
+
+            // assert
+            var expected = 1977;
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void FirstYearSithLordWasIntroduced_Return_1977_UsingExtensionMethod()
+        {
+            // act
+            var actual = Queries.GetYearTheFirstSithLordWasIntroducedExtensionMethod();
+
+            // assert
+            var expected = 1977;
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void GetUniqueListOfWizardsFromHarryPotterExtensionMethod_Return_4_wizards_as_tuple()
+        {
+            // act
+            var actual = Queries.GetUniqueListOfWizardsFromHarryPotterExtensionMethod();
+
+            // assert
+            var expected = new (string,int?)[]{("Harry Potter",2001),("Ron Weasley",2001),("Albus Dumbledore",2002),("Draco Malfoy",2002)};
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void GetUniqueListOfWizardsFromHarryPotterLinq_Return_4_wizards_as_tuple()
+        {
+            // act
+            var actual = Queries.GetUniqueListOfWizardsFromHarryPotterLinq();
+
+            // assert
+            var expected = new (string,int?)[]{("Harry Potter",2001),("Ron Weasley",2001),("Albus Dumbledore",2002),("Draco Malfoy",2002)};
+            Assert.Equal(expected,actual);
+        }
+        
+        [Fact]
+        public void GroupedByCreatorInReverseOrderByCreatorNameAndThenWizardNameExtensionMethod()
+        {
+            // act
+            var actual = Queries.GetGroupedByCreatorInReverseOrderByCreatorNameAndThenWizardNameExtensionMethod();
+
+            // assert
+            var expected = new[]
+            {
+                "Merlin",
+                "Doctor Strange",
+                "Sauron",
+                "Sam",
+                "Frodo",
+                "Ron Weasley",
+                "Harry Potter",
+                "Draco Malfoy",
+                "Albus Dumbledore",
+                "Darth Vader",
+                "Darth Sidious",
+                "Darth Maul"
+            };
+            
+            Assert.Equal(expected,actual);
+        }
+        [Fact]
+        public void GroupedByCreatorInReverseOrderByCreatorNameAndThenWizardNameLinq()
+        {
+            // act
+            var actual = Queries.GetGroupedByCreatorInReverseOrderByCreatorNameAndThenWizardNameLinq();
+
+            // assert
+            var expected = new[]
+            {
+                "Merlin",
+                "Doctor Strange",
+                "Sauron",
+                "Sam",
+                "Frodo",
+                "Ron Weasley",
+                "Harry Potter",
+                "Draco Malfoy",
+                "Albus Dumbledore",
+                "Darth Vader",
+                "Darth Sidious",
+                "Darth Maul"
+            };
+            
+            Assert.Equal(expected,actual);
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Assignment3
 {
     public class Queries
     {
+        public static IEnumerable<string> GetWizardsInventedByRowlingExtensionMethod()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var wizardsFiltered = wizards.Where(w => w.Creator.Contains("Rowling")).Select(w => w.Name); 
+            return wizardsFiltered;
+        }
+        
+        public static IEnumerable<string> GetWizardsInventedByRowlingLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var wizardsFiltered = from w in wizards where w.Creator.Contains("Rowling") select w.Name;
+            return wizardsFiltered;
+        }
+
+        public static int? GetYearTheFirstSithLordWasIntroducedExtensionMethod()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var firstYear = wizards.Where(w => w.Name.Contains("Darth")).OrderBy(w => w.Year).First().Year;
+            return firstYear;
+        }
+        
+        public static int? GetYearTheFirstSithLordWasIntroducedLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var firstYear = (from w in wizards where w.Name.Contains("Darth") orderby w.Year select w.Year).First();
+            return firstYear;
+        }
+        
+        public static IEnumerable<(string,int?)> GetUniqueListOfWizardsFromHarryPotterExtensionMethod()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var filteredWizards = wizards.Where(w => w.Medium.Contains("Harry Potter"))
+                .Select(w => (w.Name, w.Year)).Distinct();
+            return filteredWizards;
+        }
+        
+        public static IEnumerable<(string,int?)> GetUniqueListOfWizardsFromHarryPotterLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var filteredWizards = (from w in wizards where w.Medium.Contains("Harry Potter") select (w.Name, w.Year)).Distinct();
+            return filteredWizards;
+        }
+        
+        public static IEnumerable<string> GetGroupedByCreatorInReverseOrderByCreatorNameAndThenWizardNameExtensionMethod()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var filteredWizards = wizards.GroupBy(w => (w.Creator, w.Name)).OrderBy(w => w.Key.Creator)
+                .ThenBy(w => w.Key.Name).Reverse().Select(w => w.Key.Name);
+            return filteredWizards;
+        }
+        
+        public static IEnumerable<string> GetGroupedByCreatorInReverseOrderByCreatorNameAndThenWizardNameLinq()
+        {
+            var wizards = Wizard.Wizards.Value;
+            var filteredWizards = (from w in wizards
+                group w by (w.Creator, w.Name)
+                into newGroup
+                orderby (newGroup.Key.Creator, newGroup.Key.Name)
+                select newGroup.Key.Name).Reverse();
+            return filteredWizards;
+        }
     }
 }
